ba55798450e04e78a6858cfaa047f60c799ffb14
adverma98 <av558@scarletmail.rutgers.edu>
2019-04-22 17:29:38 -0400

    Made GUI prettier :)

--- TestGUI.py
+++ TestGUI.py
@@ -14,20 +14,27 @@ red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
 
-gameDisplay = pygame.display.set_mode((display_width,display_height)) #set_mode((width x height)) <== one parameter that is a tuple
+oof = (69,69,69)
+bkg = (175,200,238)
 
+gameDisplay = pygame.display.set_mode((display_width,display_height)) #set_mode((width x height)) <== one parameter that is a tuple
+soeImg = pygame.image.load('pics/rutgers-soe-logo-2.png')
+
+def logo():
+    gameDisplay.blit(soeImg, (display_width/2+50, display_height-150))
 
 def text_objects(text, font):
-    textSurface = font.render(text, True, red)
+    textSurface = font.render(text, True, oof)
     return textSurface, textSurface.get_rect()
 
 def show_message(msg):
     pygame.display.set_caption('RU Slouching') #name of project
-    gameDisplay.fill(white)
+    gameDisplay.fill(bkg)
     largeText = pygame.font.Font('freesansbold.ttf',95)
     textSurf, textRect = text_objects(msg, largeText)
     textRect.center = ((display_width/2), (display_height/2))
     gameDisplay.blit(textSurf, textRect)
+    logo()
     pygame.display.update()
 
 def slouching(isSlouching):
