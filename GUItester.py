94cd73b1b4d39395d09ce658f4c01583d5b4535c
adverma98 <av558@scarletmail.rutgers.edu>
2019-04-22 21:30:26 -0400

    START window

--- GUItester.py
+++ GUItester.py
@@ -1,4 +1,6 @@
 import TestGUI as tg
+import time
 
+time.sleep(3)
 tg.slouching(1)
 tg.slouching(0)
--- TestGUI.py
+++ TestGUI.py
@@ -1,8 +1,8 @@
-import pygame
+import pygame as pg
 import time
 import random
 
-pygame.init()
+pg.init()
 display_width = 800
 display_height = 600
 
@@ -13,34 +13,91 @@ white = (255,255,255)
 red = (255,0,0)
 green = (0,255,0)
 blue = (0,0,255)
-
 oof = (69,69,69)
-bkg = (175,200,238)
+bkg = (221,160,221)
 
-gameDisplay = pygame.display.set_mode((display_width,display_height)) #set_mode((width x height)) <== one parameter that is a tuple
-soeImg = pygame.image.load('pics/rutgers-soe-logo-2.png')
 
-def logo():
-    gameDisplay.blit(soeImg, (display_width/2+50, display_height-150))
+class Start:
+
+    def __init__(self, rect, command):
+        self.rect = pg.Rect(rect)
+        self.image = pg.Surface(self.rect.size).convert()
+        self.image.fill(black)
+        self.function = command
+
+    def get_event(self, event):
+        if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+            self.on_click(event)
+
+    def on_click(self, event):
+        if self.rect.collidepoint(event.pos):
+            self.function()
+
+    # def addText(self, surf):
+    #     self.surf.blit(self.font.render('START', True, (255,0,0)), (200, 100))
+    #     pg.display.update()
+
+    def draw(self, surf):
+        surf.blit(self.image, self.rect)
+        font = pg.font.Font('freesansbold.ttf',30)
+        new_game_text = font.render("START", False, red)
+        surf.blit(new_game_text, self.rect)
+        # addText(self, surf)
+
+
+
+def button_was_pressed():
+    # pg.display.set_mode((0,0))
+    done = True
+    print('button_was_pressed')
+
+screen = pg.display.set_mode((600,200))
+screen.fill((white))
+done = False
+btn = Start(rect=(250,50,100,35), command=button_was_pressed)
+
+while not done:
+
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            done = True
+            quit()
+        # btn.get_event(event)
+        if event.type == pg.MOUSEBUTTONDOWN and event.button == 1:
+            done = True
+    btn.draw(screen)
+    pg.display.update()
+
+
+
+gameDisplay = pg.display.set_mode((50, 50)) #set_mode((width x height)) <== one parameter that is a tuple
+soeImg = pg.image.load('pics/rutgers-soe-logo-2.png')
+
+
+def logo(x,y):
+    gameDisplay.blit(soeImg, (x, y))
+
+# logo(0,0)
 
 def text_objects(text, font):
     textSurface = font.render(text, True, oof)
     return textSurface, textSurface.get_rect()
 
 def show_message(msg):
-    pygame.display.set_caption('RU Slouching') #name of project
+    pg.display.set_caption('RU Slouching') #name of project
     gameDisplay.fill(bkg)
-    largeText = pygame.font.Font('freesansbold.ttf',95)
+    largeText = pg.font.Font('freesansbold.ttf',95)
     textSurf, textRect = text_objects(msg, largeText)
     textRect.center = ((display_width/2), (display_height/2))
     gameDisplay.blit(textSurf, textRect)
-    logo()
-    pygame.display.update()
+    logo(display_width/2+50, display_height-150)
+    pg.display.update()
 
 def slouching(isSlouching):
+    pg.init()
     # slouching(1)
     # slouching(0)
-    gameDisplay = pygame.display.set_mode((display_width,display_height)) #set_mode((width x height)) <== one parameter that is a tuple
+    gameDisplay = pg.display.set_mode((display_width,display_height)) #set_mode((width x height)) <== one parameter that is a tuple
     msg = ""
     if(isSlouching==1):
         msg = "slouching"
@@ -48,5 +105,5 @@ def slouching(isSlouching):
         msg = "NOT slouching"
     show_message(msg)
     time.sleep(3)
-    # pygame.display.quit()
+    # pg.display.quit()
     # quit()
